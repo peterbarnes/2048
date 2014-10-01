74af894f14b97ae5d6eba3b930c1b038ed8e9c85
peterbarnes <barnespeter213@gmail.com>
2014-10-01 15:04:48 -0500

    Start printing the board

--- src/Game2048.java
+++ src/Game2048.java
@@ -1,9 +1,43 @@
+import java.util.Scanner;
+import java.util.Random;
 
 public class Game2048 {
+	
+	public void fillSpace(int[][]table, int r, int c){
+		
+		Random rd = new Random();
+		r = rd.nextInt(4);
+		c = rd.nextInt(4);
+		if(table[r][c] == 0){
+			// TODO Add random 4
+			table[r][c] = 2;
+			
+		} else if(table[r][c] != 0){
+			
+			table[r][c] = table[r][c];
+			
+		}
+		
+	}
+	
+	public void printTable(int[][] table){
+		for(int i = 0; i < 4; i++){
+			for(int j = 0; j < 4; j++){
+				System.out.print(table[i][j] + " ");	
+			}
+			System.out.println(" ");
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		Game2048 game = new Game2048();
+		int r = 0, c = 0;
+		
+		int[][] table = new int[4][4];
+		game.fillSpace(table, r, c);
+		game.fillSpace(table, r, c);
+		game.printTable(table);
+		
+	}	
 
 }
